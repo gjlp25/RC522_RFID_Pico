f868121a0c590ff8402e22b589fc2cabaa458382
Robert Postma <robert.postma@topicus.nl>
2025-02-24 08:45:04 +0100

    Remove ignored files from repository

--- lib/secrets.py
+++
@@ -1,8 +0,0 @@
-wifi_ssid = 'your_wifi_ssid'
-wifi_password = 'your_wifi_password'
-mqtt_server = 'your_mqtt_server'
-mqtt_port = 1883
-#mqtt_topic = 'homeassistant/light/room/set'
-mqtt_client_id = 'your_mqtt_client_id'
-mqtt_user = 'your_mqtt_user'
-mqtt_password = 'your_mqtt_password'
