eeddc05bcbbd7b54a25b2a19c6241c3a84da8cd1
Robert Postma <47635871+gjlp25@users.noreply.github.com>
2025-02-25 14:37:53 +0100

    cleaned up folder structure

--- secrets.py
+++
@@ -1,8 +0,0 @@
-wifi_ssid = 'WiCaRo_IoT'
-wifi_password = 'Test@2021'
-mqtt_server = '10.0.30.29'
-mqtt_port = 1883
-mqtt_client_id = 'mfrc522'  # Ensure this line is present
-mqtt_user = 'mqtt_gjlp25'
-mqtt_password = 'o67@4ZeNSq&3k8'
-mqtt_topic = 'rc522/card'  # MQTT topic to publish messages to
